cab8968fb95b722e236ab975e413970ca9f19183
Bryn Mailer <bryndiupero@gmail.com>
2025-03-14 19:07:57 +1000

    feat: added support for wal colors to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,12 @@
 set -g fish_greeting
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    fish_vi_key_bindings
+  # Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
 
-    # Wal
-    cat ~/.cache/wal/sequences
+  # Wal colors
+  cat ~/.cache/wal/sequences
+  source ~/.cache/wal/colors.fish
 
-    alias get_idf=". $HOME/Tools/esp-idf/export.fish"
+  alias get_idf=". $HOME/Tools/esp-idf/export.fish"
 end
