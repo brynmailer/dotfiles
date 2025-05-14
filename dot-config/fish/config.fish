65b7be4a71554dd4faff5e18b0b451e49b6f4c68
brynmailer <bryndiupero@gmail.com>
2025-05-14 12:19:05 +1000

    feat: added support for nvm

--- dot-config/fish/config.fish
+++ dot-config/fish/config.fish
@@ -2,8 +2,13 @@ set -g fish_greeting
 
 set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 
-# Start flavours
-# End flavours
+# Execute Node Version Manager via Bash
+function nvm
+  set -x NVM_DIR $XDG_CONFIG_HOME/nvm
+  if test -x $NVM_DIR/nvm.sh
+    bash -c "source $NVM_DIR/nvm.sh; nvm $argv"
+  end
+end
 
 if status is-interactive
   fish_vi_key_bindings
