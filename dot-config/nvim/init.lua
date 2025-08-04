0774781eb1e4e431766ceaadf6dae43808868b7f
brynmailer <bryndiupero@gmail.com>
2025-08-04 14:42:23 +1000

    feat: auto reload open files when switching tmux window

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -2,4 +2,5 @@
 -- Load core configuration
 require("config.set")   -- Basic settings
 require("config.remap") -- Key mappings
+require("config.auto")  -- Auto commands
 require("config.lazy")  -- Plugin management with lazy.nvim
