27bd04486fe9fe4e8ddb51b6cb5d43b8fe488b0c
brynmailer <bryndiupero@gmail.com>
2026-05-28 14:52:01 +1000

    chore: misc config cleanup
    - untrack fish_variables (runtime SSH agent state)
    - drop unused docked/roaming hypr configs and the related pseudotile + togglesplit bindings
    - add markdown + injected formatters to conform.nvim

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -6,6 +6,8 @@ return {
       require("conform").setup({
         formatters_by_ft = {
           rust = { "rustfmt", lsp_format = "fallback" },
+          markdown = { "prettierd" },
+          ["_"] = { "injected" },
         },
         format_on_save = {
           timeout_ms = 500,
